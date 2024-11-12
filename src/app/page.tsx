44dddd9a0ab5562dc36829cbdff03e9bcbe7d006
unknown <dl.kokhtashvili@gmail.com>
2024-11-13 00:40:19 +0400

    add icon to trigger Create Task window with open/close functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,34 @@
+"use client";
 import Header from "../components/Header";
 import SearchNotes from "../components/SearchNotes";
 import CreateTask from "../components/CreateTask";
+import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [isCreateTaskOpen, setIsCreateTaskOpen] = useState<boolean>(false);
   return (
-    <div className="pt-[30px] px-5 bg-bgColor h-screen">
+    <div className="pt-[30px] px-5 bg-bgColor h-screen pb-28">
       <Header />
       <SearchNotes />
-      <CreateTask />
+      <CreateTask
+        isCreateTaskOpen={isCreateTaskOpen}
+        setIsCreateTaskOpen={setIsCreateTaskOpen}
+      />
+
+      <div className="flex justify-center h-full relative">
+        <div className="absolute bottom-4">
+          <Image
+            onClick={() => setIsCreateTaskOpen(true)}
+            alt="add task icon"
+            width={52}
+            height={52}
+            src="/add.png"
+          />
+        </div>
+
+        <div className="w-[133px] h-[5px] bg-[#6C86A8] mt-auto rounded-full"></div>
+      </div>
     </div>
   );
 }
--- src/components/CreateTask.tsx
+++ src/components/CreateTask.tsx
@@ -2,31 +2,49 @@ import React from "react";
 
 import Image from "next/image";
 
-export default function CreateTask() {
+interface CreateTaskProps {
+  isCreateTaskOpen: boolean;
+  setIsCreateTaskOpen: (isOpen: boolean) => void;
+}
+
+export default function CreateTask({
+  isCreateTaskOpen,
+  setIsCreateTaskOpen,
+}: CreateTaskProps) {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-500 bg-opacity-75 px-4">
-      <div className="pt-[14px] pb-3 px-3 pt- flex flex-col bg-[#F6FAFF] rounded-xl">
-        <div className="flex gap-[99px] ml-auto items-center">
-          <h1 className="font-bold text-xs text-[#30507D]">Create task</h1>
-          <div>
-            <Image alt="close icon" width={20} height={20} src="/close.png" />
+    <>
+      {isCreateTaskOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-500 bg-opacity-75 px-4">
+          <div className="pt-[14px] pb-3 px-3 pt- flex flex-col bg-[#F6FAFF] rounded-xl">
+            <div className="flex gap-[99px] ml-auto items-center">
+              <h1 className="font-bold text-xs text-[#30507D]">Create task</h1>
+              <div>
+                <Image
+                  alt="close icon"
+                  width={20}
+                  height={20}
+                  src="/close.png"
+                  onClick={() => setIsCreateTaskOpen(false)}
+                />
+              </div>
+            </div>
+
+            <form>
+              <input
+                className="w-full border-[1px] border-[#6A6CE0] rounded-lg mt-2 text-[12px] placeholder:text-[#B0B0B0] pl-[7px] py-[7px] focus:outline-none"
+                placeholder="Task Name"
+              />
+              <textarea
+                className="w-full h-[99px] bg-[#E8F1FD] mt-[10px] placeholder:text-[#6C86A8] text-[10px] font-semibold pl-3 pt-5 shadow-inner-all-sides focus:outline-none"
+                placeholder="Type task details here..."
+              />
+              <button className="bg-butoonColor px-[140px] w-full text-[#FFFEFC] bg-[#6A6CE0] text-xs py-3 rounded">
+                Save
+              </button>
+            </form>
           </div>
         </div>
-
-        <form>
-          <input
-            className="w-full border-[1px] border-[#6A6CE0] rounded-lg mt-2 text-[12px] placeholder:text-[#B0B0B0] pl-[7px] py-[7px] focus:outline-none"
-            placeholder="Task Name"
-          />
-          <textarea
-            className="w-full h-[99px] bg-[#E8F1FD] mt-[10px] placeholder:text-[#6C86A8] text-[10px] font-semibold pl-3 pt-5 shadow-inner-all-sides focus:outline-none"
-            placeholder="Type task details here..."
-          />
-          <button className="bg-butoonColor px-[140px] w-full text-[#FFFEFC] bg-[#6A6CE0] text-xs py-3 rounded">
-            Save
-          </button>
-        </form>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
