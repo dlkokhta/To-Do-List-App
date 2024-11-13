20bf95a1cdb8da30799425060d34efce367228ce
unknown <dl.kokhtashvili@gmail.com>
2024-11-13 07:20:19 +0400

    add toggle functionality to Taks component, arrow up/down tasks text visibility

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,28 +1,49 @@
 import React from "react";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Tasks() {
+  const [isTaskTextHidden, setIsTaskTextHidden] = useState<boolean>(false);
   return (
     <div className="bg-[#F6FAFF] mt-40 p-3 rounded-xl shadow-outer-all-sides">
       <div className="flex justify-between items-center">
         <h1 className="text-[#30507D] text-sm font-medium">
           Dorem ipsum dolor sit
         </h1>
-        <div>
-          <Image
-            alt="chevron-up icon"
-            width={20}
-            height={20}
-            src="/chevron-up.png"
-          />
-        </div>
+        {isTaskTextHidden && (
+          <div>
+            <Image
+              onClick={() => setIsTaskTextHidden(false)}
+              alt="chevron-up icon"
+              width={20}
+              height={20}
+              src="/chevron-up.png"
+            />
+          </div>
+        )}
+        {!isTaskTextHidden && (
+          <div>
+            <Image
+              onClick={() => setIsTaskTextHidden(true)}
+              alt="chevron-down icon"
+              width={20}
+              height={20}
+              src="/chevron-down.png"
+            />
+          </div>
+        )}
       </div>
-
-      <p className="bg-[#E8F1FD] mt-10 shadow-inner-all-sides p-3 text-[#6C86A8] font-semibold text-[10px]">
-        Borem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate
-        libero et velit consectetur adipiscing.
-      </p>
-      <div className="flex justify-between mt-[23px]">
+      {isTaskTextHidden && (
+        <p className="bg-[#E8F1FD] mt-10 shadow-inner-all-sides p-3 text-[#6C86A8] font-semibold text-[10px]">
+          Borem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+          vulputate libero et velit consectetur adipiscing.
+        </p>
+      )}
+      <div
+        className={`flex justify-between  ${
+          !isTaskTextHidden ? `mt-[36px]` : `mt-[23px]`
+        }`}
+      >
         <div className="flex gap-[10px]">
           <div>
             <Image alt="edit icon" width={20} height={20} src="/edit.png" />
